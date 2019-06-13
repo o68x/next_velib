c675c274f7b6f2f888ef911fb7f6682ca4f94c3e
Olivier Cardinaux <ocardinaux@yahoo.fr>
2019-06-13 18:41:27 +0200

    geocode search results

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @search = params[:q]
+    @locations = helpers.select_location(@search)
+  end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module HomeHelper
+  def select_location(search)
+    results = Geocoder.search(search)[0].data["features"]
+    # binding.pry
+    results.select{ |city| city["properties"]["city"] == "Paris" }
+  end
 end
