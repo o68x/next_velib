126d9bc177b543697b46669356bff34344e88533
Olivier Cardinaux <ocardinaux@yahoo.fr>
2019-06-18 10:29:56 +0200

    fix heroku cache config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,10 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :redis_cache_store, {
+    url: ENV["REDIS_URL"],
+    expires_in: 2.minutes
+  }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
