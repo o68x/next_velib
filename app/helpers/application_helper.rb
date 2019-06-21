9521c81750248cb990cbedda0750110c20e91603
Olivier Cardinaux <ocardinaux@yahoo.fr>
2019-06-21 15:12:45 +0200

    add dynamic page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "Vélibs"
+    if page_title.empty?
+      base_title + " ?"
+    else
+      base_title + "  vers " + page_title
+    end
+  end
 end
